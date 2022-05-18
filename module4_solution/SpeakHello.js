4de2f668ea9470f6f6a8f56be9666ded51c8c710
drstrange1410 <85047854+drstrange1410@users.noreply.github.com>
2022-05-18 23:29:07 +0530

    Update SpeakHello.js

--- module4_solution/SpeakHello.js
+++ module4_solution/SpeakHello.js
@@ -22,6 +22,7 @@ helloSpeaker.speak = function(name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+window.helloSpeaker = helloSpeaker;
+  
 })(window);
   
